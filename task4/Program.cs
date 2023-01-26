fc4fda11cccdd6a60c65316139bcbc702ff2ff48
KosachevAN <msmand@yandex.ru>
2023-01-26 21:24:31 +0500

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.Write("Введите число 1: ");
+int max = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 3: ");
+int l = Convert.ToInt32(Console.ReadLine());
+
+if (m > max) max = m;
+if (l > max) max = l;
+    
+Console.WriteLine($"Максимальное число = {max}");
