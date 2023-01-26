c09c2f71416c49413d6192cd46d77b83fcc824ea
KosachevAN <msmand@yandex.ru>
2023-01-26 21:57:59 +0500

    task8

---
+++ task8/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()), count = 2;
+while (count <= n)
+{
+    Console.Write($"{count} ");
+    count = count+2;
+}
