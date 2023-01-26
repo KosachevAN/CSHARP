3585f80b43314f101ddece70bef2a32517af3433
KosachevAN <msmand@yandex.ru>
2023-01-26 21:48:12 +0500

    task6

---
+++ task6/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int d = n % 2;
+if (d == 0) Console.WriteLine("четное");
+else Console.WriteLine("нечетное");
