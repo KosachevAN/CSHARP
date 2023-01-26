56fee9ec727ef828c2a5ff02df6bd794e181c404
KosachevAN <msmand@yandex.ru>
2023-01-26 21:10:59 +0500

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.Write("Введите 1-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (n > m)
+    Console.WriteLine($"{n} > {m}");
+else if (n < m)
+    Console.WriteLine($"{n} < {m}");
+else 
+    Console.WriteLine($"{n} = {m}");
