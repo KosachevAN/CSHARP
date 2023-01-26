3b560b8532820f3acb678d9e2d72fe5885492662
KosachevAN <msmand@yandex.ru>
2023-01-26 17:50:55 +0500

    A - B = C

---
+++ exam2/Program.cs
@@ -0,0 +1,9 @@
+﻿int A = new Random().Next(1,10);
+int B = new Random().Next(1,10);
+int C = 0;
+if ( A > B ) C = A - B ;
+if ( B > A ) C = B - A ;
+Console.WriteLine(A);
+Console.WriteLine(B);
+Console.WriteLine("A - B = C");
+Console.WriteLine(C);
