ec921739fdf280b4039ca3f80555a363db78ab92
KosachevAN <msmand@yandex.ru>
2023-01-26 17:19:32 +0500

    rand

--- exam1/Program.cs
+++ exam1/Program.cs
@@ -1,4 +1,9 @@
-﻿Console.Write("ИМЯ : ");
-string username = Console.ReadLine();
-Console.Write("Привет, ");
-Console.Write(username);
+﻿//Console.Write("ИМЯ : ");
+//string username = Console.ReadLine();
+//Console.Write("Привет, ");
+//Console.Write(username);
+int A = new Random().Next(1,10);
+int B = new Random().Next(1,10);
+Console.WriteLine(A);
+Console.WriteLine(B);
+Console.WriteLine(A+B);
