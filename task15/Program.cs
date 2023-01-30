85a73cc3ab79dd332c8be9c9c0bfeaaef88f39f1
KosachevAN <msmand@yandex.ru>
2023-01-30 22:04:31 +0500

    task15

---
+++ task15/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+
+Console.Write("Введите день недели: ");
+int d = Convert.ToInt32(Console.ReadLine());
+
+if ((d == 6) || (d == 7)) 
+    Console.WriteLine("Да");
+else
+    Console.WriteLine("Нет");
