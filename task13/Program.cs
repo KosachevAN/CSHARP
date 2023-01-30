360d793761f854c1e9b6b9b34ad19d9b2c92d1df
KosachevAN <msmand@yandex.ru>
2023-01-30 21:40:50 +0500

    task13

---
+++ task13/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+String str = num.ToString();
+String err = "третьей цифры нет";
+
+if (str.Length > 2) 
+    Console.WriteLine(str[2]);
+else
+    Console.WriteLine(err); 
+    
+ 
+
