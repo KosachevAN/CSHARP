4fc5945159675ad334121e26bf7923d0d40b3911
KosachevAN <msmand@yandex.ru>
2023-01-30 22:32:16 +0500

    taskadd

---
+++ taskadd/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+int n = -1;
+int n1 = 0;
+int max = 0;
+
+Console.WriteLine("Введите числа : ");
+
+while (n != 0)
+{
+  n = Convert.ToInt32(Console.ReadLine());
+  if ( n > max ) 
+    {
+        n1 = max;
+        max = n;
+    }
+
+//Console.WriteLine($"n={n} max={max} n1={n1}");
+}
+Console.WriteLine(n1);
