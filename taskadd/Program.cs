968a489c11a32a3309a396ded3a023616ca7bc69
KosachevAN <msmand@yandex.ru>
2023-02-01 13:58:29 +0500

    taskadd v2

--- taskadd/Program.cs
+++ taskadd/Program.cs
@@ -8,13 +8,12 @@ Console.WriteLine("Введите числа : ");
 
 while (n != 0)
 {
-  n = Convert.ToInt32(Console.ReadLine());
-  if ( n > max ) 
-    {
-        n1 = max;
-        max = n;
-    }
+n = Convert.ToInt32(Console.ReadLine());
+if ( n > max ) {
+  n1=max; 
+  max = n;
+} else if (n > n1) n1 = n;
 
-//Console.WriteLine($"n={n} max={max} n1={n1}");
+Console.WriteLine($"n={n} max={max} n1={n1}");
 }
 Console.WriteLine(n1);
