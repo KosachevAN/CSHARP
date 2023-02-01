4a59890271ae287ced33a83c41b8aab85f5fe6d4
KosachevAN <msmand@yandex.ru>
2023-02-01 13:14:02 +0500

    task13 v2

--- task13/Program.cs
+++ task13/Program.cs
@@ -2,13 +2,28 @@
 
 Console.Write("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
-String str = num.ToString();
-String err = "третьей цифры нет";
+int n = -1;
+int d = 1;
+int l = 0;
+int res = 0;
 
-if (str.Length > 2) 
-    Console.WriteLine(str[2]);
-else
-    Console.WriteLine(err); 
-    
- 
+num = num / 1000;
+
+while (n != 0) {
+    d = d * 10;
+    n = num / d;
+    l++;
+//Console.WriteLine($"l={l} d={d} n={n} " );
+}
+d = d / 100;
+Console.WriteLine(d);
+if ( l>2 ) {
+    res = num % d;   
+    Console.WriteLine(res);
+    d = d / 10;
+    res = res / d;    
+    //res = Convert.ToInt32(num % Math.Pow(10, l-2) / Math.Pow(10, l-3));
+    Console.WriteLine(res);
+} else
+    Console.WriteLine("третьей цифры нет");
 
