2f4fc11092b88a9350a9cc202e3f6bbfec9043e1
KosachevAN <msmand@yandex.ru>
2023-02-02 21:11:40 +0500

    task19

---
+++ task19/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите палиндром: ");
+int n = Convert.ToInt32((Console.ReadLine()));
+int i, p=0, r = 0; 
+for (i = n; i > 0; i = (i / 10))
+    {
+        r = i % 10;
+        p = p*10 + r;
+        //Console.WriteLine($"r={r} i={i} p={p}");
+    }
+Console.WriteLine($"{n == p}"); 
+
+
