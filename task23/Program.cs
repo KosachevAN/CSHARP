5f41cb925e4a3a01c2ceea7cdf0dc3319728c16b
KosachevAN <msmand@yandex.ru>
2023-02-02 21:42:35 +0500

    task23

---
+++ task23/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите число : ");
+int n = Convert.ToInt32((Console.ReadLine()));
+int s = 0, i = 0;
+for (i = 1 ; i<=n; i++ ) {
+    s = i*i*i;
+    Console.WriteLine(s);
+}
